add27f9c83d1dd47626ef9007de28557dc031003
mingchuno <mingchuno@gmail.com>
2014-08-20 00:21:13 +0800

    added rm wget files

--- setup.sh
+++ setup.sh
@@ -12,12 +12,14 @@ wget http://www.scala-lang.org/files/archive/scala-2.11.2.deb
 sudo dpkg -i scala-2.11.2.deb
 sudo apt-get update
 sudo apt-get install scala
+rm scala-2.11.2.deb
 
 # Install sbt
 wget http://dl.bintray.com/sbt/debian/sbt-0.13.5.deb
 sudo dpkg -i sbt-0.13.5.deb 
 sudo apt-get update
 sudo apt-get install sbt
+rm sbt-0.13.5.deb
 
 # Install Java JDK
 sudo apt-get install python-software-properties
@@ -31,6 +33,7 @@ sudo unzip akka_2.11-2.3.5.zip -d /usr/local/akka
 sudo echo 'AKKA_HOME=/usr/local/akka/akka-2.3.5' > /etc/profile.d/akka.sh
 sudo echo 'export AKKA_HOME' >> /etc/profile.d/akka.sh
 # beware of the last 2 lines, check the *.sh ok or not
+rm akka_2.11-2.3.5.zip
 
 git clone https://github.com/mingchuno/spray-template.git
 git checkout on_spray-can_1.3
